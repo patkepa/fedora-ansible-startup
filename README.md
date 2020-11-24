40b0d1620bbcafefaa7b3a907e6b6729009ed3aa
Patryk Kępa <61762509+patryk-kepa@users.noreply.github.com>
2020-11-24 01:44:34 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# fedora-ansible-startup
+# Quick install
+
+```bash
+git clone https://github.com/patryk-kepa/fedora-ansible-startup
+sudo bash bootstrap.sh
+```
+
---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+#Install Ansible
+dnf install ansible
+
+#Launch Ansible
+ansible playbook main.yml
+
+#Get jetbrains toolbox
+wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.18.7609.tar.gz
+tar -zxvf jetbrains*.tar.gz
+cd jetbrains*/
+./jetbrains-toolbox
