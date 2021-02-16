9aac169005c908403dddbfea84047400064eb451
Patryk Kępa <61762509+patryk-kepa@users.noreply.github.com>
2021-02-16 18:14:02 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,9 @@
 
 #Install Ansible
 dnf install ansible
+ansible-galaxy collection install community.general
 
 #Launch Ansible
-ansible playbook main.yml
+ansible-playbook main.yml
 
-#Get jetbrains toolbox
-wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.18.7609.tar.gz
-tar -zxvf jetbrains*.tar.gz
-cd jetbrains*/
-./jetbrains-toolbox
+
